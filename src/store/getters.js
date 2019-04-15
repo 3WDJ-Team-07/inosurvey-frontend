e4fcc8979e3db6216f2c35952328072fabb8ba21
hojin-chu <cnghwls12@naver.com>
2019-04-16 02:04:21 +0900

    getters

--- src/store/getters.js
+++ src/store/getters.js
@@ -1,5 +1,4 @@
 const getters = {
-<<<<<<< HEAD
   isAuth(state) { 
     return !!state.token
   },
@@ -8,10 +7,6 @@ const getters = {
   },
   formLength(state) {
     return typeof state.form.list !== 'undefined' && typeof state.form.list === 'object' &&Object.keys(state.form.list).length
-=======
-  isAuth(state){  // test
-    return !!state.token
->>>>>>> 5cb614f28070daba654a5095bd4b1ba712ed83f1
   }
 }
 
